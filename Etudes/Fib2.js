1ad547a1eabeae47877af8a3591e0f9b40a26324
Andrew Miller <jazznerd206@icloud.com>
2021-01-25 10:05:01 -0800

    two way fib sequence

---
+++ Etudes/Fib2.js
@@ -0,0 +1,30 @@
+const fibonacci = n =>
+    [...Array(n)].reduce(
+        (acc, val, i) => acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i), []
+    )
+
+    
+const fibonacci2 = n => {  
+    let arr = []; 
+    let obj = {};
+    let i = 0;
+    while (i < n) {
+        if (i > 1) {
+            obj[i] = obj[i-1] + obj[i-2];
+            i++;
+        } else {
+            obj[i] = i;
+            i++;
+        }
+    }
+    for (let key in obj) {
+        arr.push(obj[key])
+    }
+    return arr;
+}
+
+    
+let time1 = Date.now()
+console.log(fibonacci(7));
+let time2 = Date.now()
+console.log((time2 - time1) / 1000 + ' seconds')
